5221c7d575d918bf877a83777c93b0a39d91377d
jmmestre <mestre.josemario@yahoo.com.br>
2022-06-23 18:17:33 -0300

    Commit inicial

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.26.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  project = "labmestrecloud"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
